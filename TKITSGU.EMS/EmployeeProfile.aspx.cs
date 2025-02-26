a63c1d24684d0152c7f631967ad1d1761fc68272
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-02-26 11:32:45 +0530

    dot net project day 2

---
+++ TKITSGU.EMS/EmployeeProfile.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace TKITSGU.EMS
+{
+    public partial class EmployeeProfile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getdata();
+        }
+
+        protected void getdata()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister where Srno=@id", con);
+            cmd.Parameters.AddWithValue("@id", Request.QueryString["id"]);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                sdr.Read();
+                
+                txtname.Text = sdr.GetValue(1).ToString();
+                txtEmail.Text = sdr.GetValue(2).ToString();
+                txtContact.Text = sdr.GetValue(3).ToString();
+                ddldept.SelectedItem.Text = sdr.GetValue(4).ToString();
+            }
+        }
+    }
+}
